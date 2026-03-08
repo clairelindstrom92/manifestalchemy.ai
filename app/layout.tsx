1fb64b3d5438ce877faccff2c9805fa4d188dd65
Claire Lindstrom <90209948+clairelindstrom92@users.noreply.github.com>
2026-03-08 16:07:55 -0400

    Add ServiceWorkerRegistration component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import GleamyWrapper from "@/components/GleamyWrapper";
 import { ToastProvider } from "@/components/shared/Toast";
+import ServiceWorkerRegistration from "@/components/ServiceWorkerRegistration";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -88,6 +89,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ServiceWorkerRegistration />
         <ToastProvider>
           <GleamyWrapper>
             {children}
